13eeedcd1d63310fb121ac86aab1983a7895cf19
上山良大 <ueyamaryoudai@ueyamaryoudainoMacBook-Pro.local>
2020-04-21 14:35:31 +0900

    d

---
+++ innovate_project/settings/heroku.py
@@ -0,0 +1,10 @@
+from .common import *
+
+import dj_database_url
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES['default']= dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
